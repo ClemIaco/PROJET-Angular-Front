2a6f3271c2e261ad4af92656c13e85b5d8b0aa5e
Clement IACOBUCCI <clement.iacobucci@gmail.com>
2021-01-13 20:12:27 +0100

    feat(app): ajout des fichiers de conf pour heroku

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/projet'));
+app.use(express.static(__dirname + '/dist/AngularForm'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '/dist/projet/index.html');
+    res.sendFile(__dirname + '/dist/AngularForm/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
